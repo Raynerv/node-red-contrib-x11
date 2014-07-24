fbf19b0f9790c0625392dc24edf09a50dc1e7908
Rayner Vintervoll <raynerv@gmail.com>
2014-07-24 11:37:00 +0200

    Refactored

--- x11.js
+++ x11.js
@@ -17,46 +17,61 @@
 module.exports = function (RED) {
     var xManager = require('./lib/xEventManager.js');
 
+    var handleMsg = function(msg) {
+        if (msg != null && msg.payload != null) {
+            console.log("Got message");
+            // { event, data }
+            var payload = msg.payload;
+            try {
+                switch(payload.event) {
+                    case 'keyUp':
+                        manager.keyUp(payload.data.key);
+                        console.log("keyUp");
+                        break;
+                    case 'keyDown':
+                        manager.keyDown(payload.data.key);
+                        console.log("keyDown");
+                        break;
+                    case 'move':
+                        manager.move(payload.data.xPercent, payload.data.yPercent);
+                        console.log("move");
+                        break;
+                    case 'moveRelative':
+                        manager.moveRelative(payload.data.x, payload.data.y);
+                        console.log("moveRelative");
+                        break;
+                    case 'click':
+                        manager.click(payload.data.clickCode);
+                        console.log("click");
+                        break;
+                }
+            } catch(err){ctx.error(err);}
+        }
+    };
+
+    var ctx;;
+
+    var manager = null;
+
     function X11FunctionNode(n) {
         RED.nodes.createNode(this, n);
         this.name = n.name;
         this.x11 = n.x11;
-        var ctx = this;
+
         this.topic = n.topic;
-        this.on("input", function(msg){this.error("No XManager")});
+        ctx = this;
+        this.on("input", function(msg){
+            if(manager == null) this.error("No XManager");
+            handleMsg(msg);
+        });
         try {
-            xManager.createXManager(function(manager) {
-                ctx.on("input", function (msg) {
-                    if (msg != null && msg.payload != null) {
-                        // { event, data }
-                        var payload = msg.payload;
-                        try {
-                            switch(payload.event) {
-                                case 'keyUp':
-                                    manager.keyUp(payload.data.key);
-                                    break;
-                                case 'keyDown':
-                                    manager.keyDown(payload.data.key);
-                                    break;
-                                case 'move':
-                                    manager.move(payload.data.xPercent, payload.data.yPercent);
-                                    break;
-                                case 'moveRelative':
-                                    manager.moveRelative(payload.data.x, payload.data.y);
-                                    break;
-                                case 'click':
-                                    manager.click(payload.data.clickCode);
-                                    break;
-                            }
-                        } catch(err){ctx.error(err);}
-                    }
-                });
-            });
+            xManager.createXManager(function(mng) { manager = mng;});
         } catch (err) {
             ctx.error(err);
+            console.err(err);
         }
     }
 
     RED.nodes.registerType("x11", X11FunctionNode);
     RED.library.register("x11");
-}
+};
